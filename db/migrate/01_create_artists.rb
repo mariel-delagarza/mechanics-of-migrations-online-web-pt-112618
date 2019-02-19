7b87db5c661d433301d881514a7da750f79a238e
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-19 17:37:31 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -5,15 +5,6 @@ class CreateArtists < ActiveRecord::Migration
   def down
   end
   
-  def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end 
-  end
-  
   ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
@@ -30,5 +21,16 @@ class CreateArtists < ActiveRecord::Migration
   SQL
  
   ActiveRecord::Base.connection.execute(sql)
+  
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end 
+  end
+  
+
 
 end
